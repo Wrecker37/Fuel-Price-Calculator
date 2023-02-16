7c51d587fdb00937e84ef41ac7f7151155b7530f
Court <93003818+counguyen@users.noreply.github.com>
2023-02-16 16:46:14 -0600

    duplicated login -> register, added some styling

--- frontend/src/routes/login.jsx
+++ frontend/src/routes/login.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, Field } from 'formik';
 import './root.css';
 import { Link } from "react-router-dom";
+import './login.css';
+
+function validateUser(value) {
+    let error;
+    if (value==="courtney") {
+        error="no thanks";
+    }
+    return error;
+}
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -12,32 +21,32 @@ export default class Login extends React.Component {
     render() {
         return (
             <>
-                <Formik initialValues={{numGallons:'', deliveryAddress: 'I-45'}} onSubmit={(values, {setSubmitting}) => {
+                <Formik initialValues={{user:'', password: ''}} onSubmit={(values, {setSubmitting}) => {
                     setTimeout(() => {
                         alert(JSON.stringify(values, null, 2));
                         setSubmitting(false);
                     }, 400);
                 }}>
 
-                    {({isSubmitting}) => (
+                    {({errors, touched, isValidating, isSubmitting}) => (
                         <div>
                             <h1>Account Login</h1>
                             <Form>
                                 <div>
-                                    <label name="user">Username: </label>
-                                    <Field type="text" name="user" placeholder="username"/>
+                                    <label for="user">Username</label>
+                                    <Field type="text" name="user" class="textbox" placeholder="username" validate={validateUser}/>
+                                    {errors.user && touched.user && <div class="error">{errors.user}</div>}
                                 </div>
                                 <div>
-                                    <label name="password">Password: </label>
-                                    <Field type="password" name="password" placeholder="password"/>
+                                    <label for="password">Password</label>
+                                    <Field type="password" name="password" class="textbox" placeholder="password"/>
                                 </div>
-                                <div>
+                                <div class="submit">
                                     <button type="submit">Submit</button>
                                 </div>
                             </Form>
-
-                            <div>
-                            <Link to={`/register`}>Register New User</Link>
+                            <div class="register">
+                                <Link to={`/register`}>Register new user &#8594;</Link>
                             </div>
                         </div>
                     )}
--- frontend/src/routes/register.jsx
+++ frontend/src/routes/register.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Formik, Form, Field } from 'formik';
+import './root.css';
+import { Link } from "react-router-dom";
+import './login.css';
 
 export default class Login extends React.Component {
     constructor(props) {
@@ -9,7 +13,36 @@ export default class Login extends React.Component {
     render() {
         return (
             <>
-                im the registration page!!!
+                <Formik initialValues={{user:'', password: ''}} onSubmit={(values, {setSubmitting}) => {
+                    setTimeout(() => {
+                        alert(JSON.stringify(values, null, 2));
+                        setSubmitting(false);
+                    }, 400);
+                }}>
+
+                    {({isSubmitting}) => (
+                        <div>
+                            <h1>Registration</h1>
+                            <Form>
+                                <div>
+                                    <label for="user">Username</label>
+                                    <Field type="text" name="user" class="textbox" placeholder="username"/>
+                                </div>
+                                <div>
+                                    <label for="password">Password</label>
+                                    <Field type="password" name="password" class="textbox" placeholder="password"/>
+                                </div>
+                                <div class="submit">
+                                    <button type="submit">Submit</button>
+                                </div>
+                            </Form>
+                            <div class="register">
+                                <Link to={`/login`}>Sign in &#8594;</Link>
+                            </div>
+                        </div>
+                    )}
+                
+                </Formik>
             </>
         );
     }
