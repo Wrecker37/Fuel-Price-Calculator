407d165bf2ac9b33fa186718715b5e5e2584ce2b
Anil Shanker <anilshanker2@gmail.com>
2023-03-18 23:01:53 -0500

    Connect Quote History page to BE

--- backend/services/quote-service.js
+++ backend/services/quote-service.js
@@ -1,7 +1,26 @@
 class QuoteService {
     static async getQuotes(userId) {
         /* DB call here */
-        const quotes = ['dummy quote'];
+        const quotes = [
+            {
+                isInState: true,
+                isPastClient: true,
+                deliveryDate: "1/13/2023",
+                gallons: 145,
+                deliveryAddress: "123 Main St.",
+                price: 3.75,
+                total: 456,
+            },
+            {
+                isInState: false,
+                isPastClient: true,
+                deliveryDate: "5/23/2023",
+                gallons: 176,
+                deliveryAddress: "345 Tiger St.",
+                price: 7,
+                total: 1000,
+            }
+        ];
 
         return quotes;
     }
--- frontend/src/routes/quotehistory.jsx
+++ frontend/src/routes/quotehistory.jsx
@@ -3,11 +3,13 @@ import { Formik, Form, Field } from 'formik';
 import './root.css';
 import { Link } from "react-router-dom";
 import * as Yup from 'yup';
-import { useState } from "react";
-import {useFormikContext} from 'formik';
+import { useState, useEffect } from "react";
+import { useFormikContext } from 'formik';
 import DatePicker from 'react-modern-calendar-datepicker';
 import Calendar from 'react-modern-calendar-datepicker'
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import { Quote } from "../components/quote";
+import QuoteService from '../services/quote-service';
 
 const valid = Yup.object().shape({
     user: Yup.string()
@@ -19,33 +21,33 @@ const valid = Yup.object().shape({
 
 function MyCalendar() {
     const [selectedDate, setSelectedDate] = useState(null);
-  
+
     const handleDateChange = (date) => {
-      setSelectedDate(date);
+        setSelectedDate(date);
     };
-  
+
     return (
-      <Calendar
-        value={selectedDate}
-        onChange={handleDateChange}
-        calendarClassName="my-calendar"
-        shouldHighlightWeekends
-      />
+        <Calendar
+            value={selectedDate}
+            onChange={handleDateChange}
+            calendarClassName="my-calendar"
+            shouldHighlightWeekends
+        />
     );
 };
 function MyDatePicker({ name, ...rest }) {
     const { setFieldValue } = useFormikContext();
 
     return (
-      <DatePicker
-        {...rest}
-        inputPlaceholder="Select a date"
-        value={rest.value}
-        onChange={(date) => setFieldValue(name, date)}
-        shouldHighlightWeekends
-      />
+        <DatePicker
+            {...rest}
+            inputPlaceholder="Select a date"
+            value={rest.value}
+            onChange={(date) => setFieldValue(name, date)}
+            shouldHighlightWeekends
+        />
     );
-  }
+}
 
 const myString = "4401 Cougar Village Dr, Houston, TX 77204";
 const price = 2.50;
@@ -54,62 +56,46 @@ const total = price * gallons;
 
 
 
-export default class QuoteHistory extends React.Component {
+const QuoteHistory = (props) => {
+    const [quotes, setQuotes] = useState([]);
 
-    constructor(props) {
-        super(props);
-        this.state = {value: ''};
-        
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const newQuotes = await QuoteService.getQuoteHistory();
+                setQuotes(newQuotes);
+                console.log(newQuotes);
+                console.log('Fetched quotes from backend!');
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchData();
+    }, [])
 
-    render() {
+    return (
+        <>
+            <Formik initialValues={{ gallons: '', address: '', selectedDay: new Date() }} validationSchema={valid} onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                    alert(JSON.stringify(values, null, 2));
+                    setSubmitting(false);
+                }, 400);
+            }}>
 
-            return (
+                {({ errors, touched, isValidating, isSubmitting }) => (
+                    <div>
+                        <h1>Quote History</h1>
+                        {quotes.map((quote, index) =>
+                            <>
+                                <h2>Quote</h2>
+                                <Quote key={index} {...quote} />
+                            </>)}
+                    </div>
+                )}
 
-                <>
-                    <Formik initialValues={{ gallons: '', address: '', selectedDay: new Date() }} validationSchema={valid} onSubmit={(values, { setSubmitting }) => {
-                        setTimeout(() => {
-                            alert(JSON.stringify(values, null, 2));
-                            setSubmitting(false);
-                        }, 400);
-                    }}>
-
-                        {({ errors, touched, isValidating, isSubmitting }) => (
-                            <div>
-                                <h1>Quote History</h1>
-                                <h2>Example 1</h2>
-                                <Form>                                 
-                                    <div>
-                                        <label for="in-state">In-State: No</label>
-                                        <label for="past-client">Past Client: Yes</label>
-                                        <label for ="date">Delivery Date: 1/13/2023</label>
-                                        <label for="gallons">Gallons Requested: {gallons+10}</label>
-                                        <label for="nonEditable" title={myString}>Delivery Address: {"107 Oklahoma 77S, Marietta, OK 73448"}</label>
-                                        {/* <label for="nonEditable">Non-Editable Address from Profile</label> */}
-                                        <label for="price">Price: ${price+.25}  / gallon </label> 
-                                        <label for="price">Total: ${total+25} </label>                                    
-                                    </div>
-                                </Form>
-                                <h2>Example 2</h2>
-                                <Form>                                 
-                                    <div>
-                                        <label for="in-state">In-State: Yes</label>
-                                        <label for="past-client">Past Client: Yes</label>
-                                        <label for ="date">Delivery Date: 2/20/2023</label>
-                                        <label for="gallons">Gallons Requested: {gallons}</label>
-                                        <label for="nonEditable" title={myString}>Delivery Address: {myString}</label>
-                                        {/* <label for="nonEditable">Non-Editable Address from Profile</label> */}
-                                        <label for="price">Price: ${price}  / gallon </label> 
-                                        <label for="price">Total: ${total} </label>                                    
-                                    </div>
-                                </Form>
-                               
-                            </div>
-                        )}
-
-                    </Formik>
-                </>
-            );
-
-    }
+            </Formik>
+        </>
+    );
 }
+
+export default QuoteHistory;
