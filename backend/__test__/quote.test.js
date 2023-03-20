1457bc79b2f0523fb6f2e0df109a22ae873f1bea
Blob28895 <blob28895@gmail.com>
2023-03-20 18:34:39 -0500

    changed to use code coverage reports

--- backend/__test__/quote.test.js
+++ backend/__test__/quote.test.js
@@ -1,7 +1,6 @@
-import {render , screen} from "@testing-library/react";
-import quote from "./quote";
-
+const add = (a, b) => a + b;
 
 test("temp", () => {
-    1 == 1;
+    const sum = add(3, 4);
+    expect(sum).toBe(7);
 })
