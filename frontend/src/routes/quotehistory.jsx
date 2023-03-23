372971a3d2f71ca154e76d12c0739d2fbec97291
Court <93003818+counguyen@users.noreply.github.com>
2023-03-23 09:05:35 -0500

    fixing 'component not found' error

--- frontend/src/routes/quotehistory.jsx
+++ frontend/src/routes/quotehistory.jsx
@@ -8,7 +8,7 @@ import { useFormikContext } from 'formik';
 import DatePicker from 'react-modern-calendar-datepicker';
 import Calendar from 'react-modern-calendar-datepicker'
 import 'react-modern-calendar-datepicker/lib/DatePicker.css';
-import { Quote } from "../components/quote";
+import { Quote } from '../components/quote';
 import QuoteService from '../services/quote-service';
 
 const valid = Yup.object().shape({
