d5927151516cdc810b76084f6647a2473182b770
Jairo Pedroza <jairo.pedroza78@gmail.com>
2023-03-24 10:59:57 -0500

    sql database setup based on UML diagram

---
+++ database/db-setup.sql
@@ -0,0 +1,41 @@
+DROP DATABASE FUELCALCULATOR;
+
+CREATE DATABASE FUELCALCULATOR;
+
+USE FUELCALCULATOR;
+
+CREATE TABLE `User` ( 
+    userID varchar(64) NOT NULL PRIMARY KEY, 
+    username varchar(64) NOT NULL,
+    passwordHash varchar(64) NOT NULL
+) ENGINE=INNODB;
+
+CREATE TABLE `Profile` (
+	ProfileID varchar(64) NOT NULL PRIMARY KEY,
+    UserID varchar(64) NOT NULL,
+    FirstName varchar(64) NOT NULL, 
+    LastName varchar(64) NOT NULL,
+    Email varchar(64) NOT NULL,
+    isExistingCustomer bool,
+    FOREIGN KEY (UserID) REFERENCES User(userID) ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE `Quote`(
+	QuoteID varchar(64) NOT NULL PRIMARY KEY,
+    UserID varchar(64) NOT NULL,
+    DateRequested DateTime(6) NOT NUll,
+    GallonsRequested float DEFAULT 0,
+    ProfitMarginPercent float DEFAULT 0,
+    FOREIGN KEY (UserID) REFERENCES User(UserID) ON DELETE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE `Address`(
+	AddressID varchar(64) NOT NULL PRIMARY KEY,
+    ProfileID varchar(64) NOT NULL,
+	Address varchar(64) NOT NULL,
+    City varchar(64) NOT NULL,
+    State varchar(64) NOT NULL,
+    Country varchar(64) NOT NULL,
+    ZipCode varchar(64) NOT NULL,
+    FOREIGN KEY (ProfileID) REFERENCES Profile(ProfileID) ON DELETE CASCADE
+) ENGINE=INNODB;
