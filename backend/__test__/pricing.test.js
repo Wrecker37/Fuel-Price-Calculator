7e8b0189edc2781860bf44c672e48573b07b8009
Anil Shanker <anilshanker2@gmail.com>
2023-04-27 23:54:44 -0500

    Recommit commit

---
+++ backend/__test__/pricing.test.js
@@ -0,0 +1,35 @@
+const PricingService = require('../services/pricing-service.js');
+const { getAddress, getProfile } = require('../db');
+
+test('In TX, isExistingCustomer, gallons > 1000', async () => {
+    const gallons = 2000;
+    const userId = 1;
+    
+    let margin = 0.1 + 0.02 - 0.01 + 0.02;
+    margin *= 1.5;
+    correctPrice = (margin + 1.5) * gallons;
+    
+    expect(await PricingService.calculatePrice(userId, gallons)).toEqual(correctPrice);
+});
+
+test('Not in TX, isExistingCustomer, gallons > 1000', async () => {
+    const gallons = 2000;
+    const userId = 3;
+    
+    let margin = 0.1 + 0.04 - 0.01 + 0.02;
+    margin *= 1.5;
+    correctPrice = (margin + 1.5) * gallons;
+    
+    expect(await PricingService.calculatePrice(userId, gallons)).toEqual(correctPrice);
+});
+
+test('In TX, isExistingCustomer, gallons <= 1000', async () => {
+    const gallons = 1000;
+    const userId = 1;
+    
+    let margin = 0.1 + 0.02 - 0.01 + 0.03;
+    margin *= 1.5;
+    correctPrice = (margin + 1.5) * gallons;
+    
+    expect(await PricingService.calculatePrice(userId, gallons)).toEqual(correctPrice);
+});
