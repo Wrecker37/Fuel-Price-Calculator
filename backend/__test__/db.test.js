3c942b95615bf03c6ddfa23b03fc8e2b35ef8fa6
Anil Shanker <anilshanker2@gmail.com>
2023-04-28 00:26:59 -0500

    Add unit test

--- backend/__test__/db.test.js
+++ backend/__test__/db.test.js
@@ -1,7 +1,13 @@
-const { addQuotes } = require('../db');
+const { addQuotes, addProfiles } = require('../db');
 
 test('getQuotes() with wrong userI', async () => {
     return expect(async () => {
         await addQuotes()
+    }).rejects.toThrow();
+});
+
+test('addProfiles() fails', async () => {
+    return expect(async () => {
+        await addProfiles()
     }).rejects.toThrow();
 });
